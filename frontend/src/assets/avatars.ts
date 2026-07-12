eff3064074dae66b8c0e30c0732f493709f4796f
Josefa <jmolguin@miuandes.cl>
2026-07-12 07:55:59 -0400

    fix(frontend): update avatar ring colors to match design specifications

--- frontend/src/assets/avatars.ts
+++ frontend/src/assets/avatars.ts
@@ -4,4 +4,4 @@ import vale from './avatar-vale.png';
 
 export const AVATAR: Record<UserName, string> = { Jo: jo, Vale: vale };
 
-export const RING: Record<UserName, string> = { Jo: '#7E9471', Vale: '#B0687A' };
+export const RING: Record<UserName, string> = { Jo: '#00E054', Vale: '#FF2834' };
